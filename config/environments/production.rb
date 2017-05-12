fb3c7a9599685265ab464d0c67a21df0cf1a1882
Your Name <you@example.com>
2017-05-12 13:37:40 -0400

    show please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.serve_static_assets = true
+config.assets.compile = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
