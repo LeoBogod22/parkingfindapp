51060af9a1fbfdd1d674def3a71d6a9c0c6021ac
Leo Bogod <combatstriker111@gmail.com>
2017-06-03 17:55:15 +0300

    Update user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  
+    test 'valid user' do
+    user = User.new(name: 'John', email: 'john@example.com')
+    assert user.valid?
+  end
+
+  test 'invalid without name' do
+    user = User.new(email: 'john@example.com')
+    refute user.valid?, 'user is valid without a name'
+    assert_not_nil user.errors[:name], 'no validation error for name present'
+  end
+
+  test 'invalid without email' do
+    user = User.new(name: 'John')
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
 end
