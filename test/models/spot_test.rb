5e60346687b12c4228019b38ac548a12c33e569c
Leo Bogod <combatstriker111@gmail.com>
2017-06-03 18:02:17 +0300

    Update spot_test.rb

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -4,4 +4,25 @@ class SpotTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  
+    def setup
+    @spot = Spot.new(title: 'John Street', address: "90 John street", phonenumber: '15184059043')
+  end
+
+  test 'valid spot' do
+    assert @spot.valid?
+  end
+
+  test 'invalid without title' do
+    @spot.title = nil
+    refute @spot.valid?, 'saved spot without a title'
+    assert_not_nil @spot.errors[:title], 'no validation error for name present'
+  end
+
+  test 'invalid without address' do
+    @spot.address = nil
+    refute @spot.valid?
+    assert_not_nil @spot.errors[:address]
+  end
 end
